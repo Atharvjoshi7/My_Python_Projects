a2021c2045a95556f4f69763883c90a8997f8f92
ATHARV JOSHI <atharvajoshi2017@gmail.com>
2024-06-05 19:36:43 +0530

    Add files via upload

---
+++ calculator_basic.py
@@ -0,0 +1,12 @@
+
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter a number: "))
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+division = num1 / num2
+
+print(addition)
+print(subtraction)
+print(multiplication)
+print(division)
