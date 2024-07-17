a664cfddd5d7b5652d345d0f6fc4b8ece45f2b8c
ATHARV JOSHI <atharvajoshi2017@gmail.com>
2024-07-17 17:02:05 +0530

    Create Bill_calculator.py

---
+++ Bill_calculator.py
@@ -0,0 +1,11 @@
+
+Bill = input("Enter the bill amount: ")
+numberOfPeople = int(input("No of peoples: "))
+tip = float(input("percentage of tip: "))
+Btip = 1 + tip/100
+
+billNum = int(Bill)
+calculate = (billNum / numberOfPeople) * Btip
+TotalBill = round(calculate)
+
+print(f"Each person should pay {TotalBill}")
