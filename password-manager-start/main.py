1cad0e0e5091b37681f5b1065e8af23272045aef
ATHARV JOSHI <atharvajoshi2017@gmail.com>
2024-08-28 15:33:02 +0530

    Update main.py

--- password-manager-start/main.py
+++ password-manager-start/main.py
@@ -3,7 +3,6 @@ from tkinter import messagebox
 import random
 import pyperclip
 
-# ---------------------------- PASSWORD GENERATOR ------------------------------- #
 def generate_password():
     letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
     numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
@@ -24,7 +23,7 @@ def generate_password():
     password_entry.insert(0, password)
     pyperclip.copy(password)
 
-# ---------------------------- SAVE PASSWORD ------------------------------- #
+
 
 def save():
     website = website_entry.get()
@@ -43,7 +42,6 @@ def save():
                 password_entry.delete(0, END)
 
 
-# ---------------------------- UI SETUP ------------------------------- #
 
 window = Tk()
 window.title("Password Manager")
@@ -79,7 +77,5 @@ generate_password_button = Button(text="Generate Password", command=generate_pas
 generate_password_button.grid(row=3, column=2)
 add_button = Button(text="Add", width=36, command=save)
 add_button.grid(row=4, column=1, columnspan=2)
-
-
 
 window.mainloop()
