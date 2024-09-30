114059881f5abba9fdccb965d6b85c66b01cb684
ATHARV JOSHI <atharvajoshi2017@gmail.com>
2024-09-30 17:48:18 +0530

    Create Video_downloader.py

---
+++ Video_downloader.py
@@ -0,0 +1,20 @@
+from asyncio import streams
+from pytube import YouTube
+import tkinter as tk
+from tkinter import filedialog
+
+def download_video(url, save_path):
+    try:
+        yt = YouTube(url)
+        streams = yt.streams.filter(progressive=True, file_extension="mp4")
+        highest_res_stream = streams.get_highest_resolution()
+        highest_res_stream.download(output_path=save_path)
+        print("Video Downloaded Successfully!")
+
+    except Exception as e:
+        print(e)
+
+url = "" # place the url of youtube video you wanna download here
+save_path = "C:/Users/hp/Downloads"
+
+download_video(url, save_path)
